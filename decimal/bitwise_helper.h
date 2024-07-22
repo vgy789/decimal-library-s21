9d59b5f080dd66bd5f22beb53d3ad0c1c9c1c5ce
Alexandr <sanek15604@gmail.com>
2024-07-22 13:03:01 +0700

    fx, simplified realization

--- decimal/bitwise_helper.h
+++ decimal/bitwise_helper.h
@@ -3,6 +3,10 @@
 
 #include "s21_decimal.h"
 
+#define DECIMAL_MAX ((s21_decimal){{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0}})
+#define DECIMAL_ONE_MINUS ((s21_decimal){{0x1, 0, 0, 0x80000000}})
+#define SCALE_MAX (28)
+
 enum sign { plus, minus };
 enum { scale_start = 112, scale_end = 119 };
 
@@ -25,7 +29,7 @@ void set_sign(s21_decimal *value, bool sign);
 /**
  * Печатает s21_decimal в двоичном предствалении.
  */
-void decimal_binary(s21_decimal value);
+void decimal_binary(s21_decimal value, bool print_scale);
 
 /**
  * Печатает unsigned int в двоичном предствалении.
--- decimal/s21_decimal.c
+++ decimal/s21_decimal.c
@@ -1,43 +1,60 @@
 #include "s21_decimal.h"
+
 #include "bitwise_helper.h"
 
 int s21_negate(s21_decimal value, s21_decimal *result) {
-  const bool sign = get_sign(value);
-  uint8_t bit_i = 0;
-
-  while (bit_i < 96) {
-    const bool bit = get_bit(value, bit_i);
-    set_bit(result, bit_i, !bit);
-    bit_i += 1;
-  }
-  const bool is_err = s21_add(*result, (s21_decimal){{0b1, 0, 0, 0}}, result);
-  set_sign(result, !sign);
-  // TODO: Возвращает ошибку. О какой ошибке идёт речь?
+  uint8_t is_err = s21_mul(value, DECIMAL_ONE_MINUS, result);
   return is_err;
 }
 
+uint8_t compliment2(s21_decimal value, s21_decimal *result) {
+  value.bits[0] = ~value.bits[0];
+  value.bits[1] = ~value.bits[1];
+  value.bits[2] = ~value.bits[2];
+  s21_inc(value, result);
+  // TODO: Возвращает ошибку. О какой ошибке идёт речь?
+  return 0;
+}
+
+static bool add_word(uint32_t *result, uint32_t value1, uint32_t value2,
+                     bool transfer) {
+  bool overflow = 0;
+  overflow += __builtin_uadd_overflow(value1, value2, result);
+  overflow += __builtin_uadd_overflow(*result, transfer, result);
+  return overflow;
+}
+
 int s21_add(s21_decimal value1, s21_decimal value2, s21_decimal *result) {
-  uint8_t bit_i = 0;
-  bool bit_transfer = false;
+  bool sign1_buf = get_sign(value1);
+  bool sign2_buf = get_sign(value2);
 
-  while (bit_i < 96) {
-    const bool bit1 = get_bit(value1, bit_i);
-    const bool bit2 = get_bit(value2, bit_i);
-    bool bit_result = bit1 ^ bit2 ^ bit_transfer;
-    set_bit(result, bit_i, bit_result);
-    bit_transfer = (bit1 & bit2) | (bit1 & bit_transfer) | (bit2 & bit_transfer);
-
-    bit_i += 1;
+  if (sign1_buf == minus) {
+    set_sign(&value1, plus);
+    compliment2(value1, &value1);
+  }
+  if (get_sign(value2) == minus) {
+    set_sign(&value2, plus);
+    compliment2(value2, &value2);
   }
 
-  // TODO: переполнение, установка знака у результата при переполнениии. Ошибка при переполнении.
+  s21_decimal buf = (s21_decimal){{0, 0, 0, 0}};
+  bool transfer = add_word(&buf.bits[0], value1.bits[0], value2.bits[0], 0);
+  transfer = add_word(&buf.bits[1], value1.bits[1], value2.bits[1], transfer);
+  transfer = add_word(&buf.bits[2], value1.bits[2], value2.bits[2], transfer);
 
-  // const bool err_overflow = bit_transfer == true && !(get_sign(value1) ^ get_sign(value2));
-  // if (err_overflow) { /* test overflow */
-  //   printf("ERROR\n");
-  // }
-  // TODO: ошибка
-  return 0;
+  uint8_t err_code = 0;
+  if (transfer) { /* переполнение | ошибка */
+  }
+
+  bool result_sign =
+      (sign1_buf == minus && sign2_buf == minus); /* (-x) + (-y) */
+  if (result_sign) {
+    compliment2(buf, &buf);
+  }
+  set_sign(&buf, result_sign);
+  *result = buf;
+
+  return err_code;
 }
 
 int s21_sub(s21_decimal value1, s21_decimal value2, s21_decimal *result) {
@@ -50,28 +67,25 @@ int s21_sub(s21_decimal value1, s21_decimal value2, s21_decimal *result) {
 int s21_mul(s21_decimal value1, s21_decimal value2, s21_decimal *result) {
   const bool sign1 = get_sign(value1);
   const bool sign2 = get_sign(value2);
-  if (sign1 == minus)
-    s21_negate(value2, &value1);
-  if (sign2 == minus)
-    s21_negate(value2, &value2);
+  if (sign1 == minus) compliment2(value2, &value1);
+  if (sign2 == minus) compliment2(value2, &value2);
 
   uint8_t is_err = 0;
   *result = (s21_decimal){0b0};
-  while(value2.bits[0] | value2.bits[1] | value2.bits[2]) {
-    s21_sub(value2, (s21_decimal){{0b1,0,0,0}}, &value2);
+  while (value2.bits[0] | value2.bits[1] | value2.bits[2]) {
+    s21_sub(value2, (s21_decimal){{0b1, 0, 0, 0}}, &value2);
     is_err = s21_add(value1, *result, result);
   }
 
   const bool sign_result = sign1 ^ sign2;
-  if (sign_result == minus)
-    s21_negate(*result, result);
-  
+  if (sign_result == minus) compliment2(*result, result);
+
   // TODO: ошибка
   return is_err;
 }
 
 // int s21_div(s21_decimal value1, s21_decimal value2, s21_decimal *result) {
-  // нужна операция сравнения >=
+// нужна операция сравнения >=
 // }
 
 bool set_scale(s21_decimal *value, uint8_t scale) {
@@ -94,11 +108,10 @@ void set_bit(s21_decimal *value, uint8_t bit_pos, bool state) {
     bit_pos -= 32;
     int_part += 1;
   }
-  if (state == false) {
+  if (state == false)
     value->bits[int_part] &= ~(1 << bit_pos);
-  } else {
+  else
     value->bits[int_part] |= (1 << bit_pos);
-  }
 }
 
 bool get_bit(s21_decimal value, uint8_t bit_pos) {
@@ -132,34 +145,41 @@ void uint_binary(uint32_t value) {
   }
 }
 
-void decimal_binary(s21_decimal value) {
+void decimal_binary(s21_decimal value, bool print_scale) {
   if (get_sign(value))
-    printf("-");
+    printf("(-)");
   else
-    printf("+");
+    printf("(+)");
+
+  if (print_scale) {
+    uint_binary(value.bits[3]);
+    printf("\'");
+  }
   uint_binary(value.bits[2]);
+  printf("\'");
   uint_binary(value.bits[1]);
+  printf("\'");
   uint_binary(value.bits[0]);
+  printf("(scale=%d)", get_scale(value));
 }
 
 void set_sign(s21_decimal *value, bool sign) {
-  if (sign == plus) {
+  if (sign == plus)
     value->bits[3] &= ~(1 << 31);
-  } else {
+  else
     value->bits[3] |= (1 << 31);
-  }
 }
 
 bool get_sign(s21_decimal value) { return ((value.bits[3] >> 31) & 1); }
 
 int s21_inc(s21_decimal value, s21_decimal *result) {
-  s21_decimal one = (s21_decimal){{1,0,0,0}};
+  const s21_decimal one = (s21_decimal){{1, 0, 0, 0}};
   s21_add(value, one, result);
   return 0;
 }
 
 int s21_dec(s21_decimal value, s21_decimal *result) {
-  s21_decimal one = (s21_decimal){{1,0,0,0}};
+  const s21_decimal one = (s21_decimal){{1, 0, 0, 0}};
   s21_sub(value, one, result);
   return 0;
 }
