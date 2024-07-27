fcf01e6c79738819514025bc16f62ff4eeb7f647
Alexandr <sanek15604@gmail.com>
2024-07-27 22:18:54 +0700

    rm files

--- decimal/others.h
+++ decimal/others.h
@@ -1,11 +1,11 @@
 #ifndef OTHERS.H
-#define OTHERS.H
+#define OTHERS .H
 
 #include "s21_decimal.h"
 
 typedef struct s21_decimal {
- int bits[4];
-}s21_decimal;
+  int bits[4];
+} s21_decimal;
 
 void mul_by_ten(s21_decimal* value);
 void div_by_ten(s21_decimal* value);
@@ -15,5 +15,5 @@ int get_scale(s21_decimal value);
 
 int get_first_num(int value, int* size);
 
-void alignment(s21_decimal* value1, s21_decimal* value2);//выравнивает мантисы
+void alignment(s21_decimal* value1, s21_decimal* value2);  //выравнивает мантисы
 #endif OTHERS.H
