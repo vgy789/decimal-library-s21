e62deb577f91bcef5166da060819585233358858
Alexandr <sanek15604@gmail.com>
2024-07-27 22:11:58 +0700

    merger, corrections

--- decimal/s21_decimal.c
+++ decimal/s21_decimal.c
@@ -1,6 +1,90 @@
 #include "s21_decimal.h"
 
+#include <math.h>
+
 #include "bitwise_helper.h"
+
+bool mul_by_ten_bin(s21_decimal *value) {
+  uint8_t scale = get_scale(*value);
+  bool is_err = s21_mul(*value, (s21_decimal){{10, 0, 0, 0}}, value);
+  set_scale(value, ++scale);
+  return is_err;
+}
+
+int get_first_num(int value, int *size) {
+  int rez = 0;
+  *size = 0;
+  while (value > 0) {
+    rez = value % 10;
+    value /= 10;
+    (*size)++;
+  }
+  (*size) -= 1;
+  return rez;
+}
+
+void mul_by_ten(s21_decimal *value) {
+  int buff = 0;
+  int zer_buff = 0;
+  int scale = get_scale(*value);
+  for (int i = 0; i <= 2; i++) {
+    if (value->bits[i] > 0xFFFFFFF / 10) {
+      buff = get_first_num(value->bits[i], &zer_buff);
+      value->bits[i] -= buff * pow(10, zer_buff);
+      value->bits[i] *= 10;
+    } else {
+      value->bits[i] = value->bits[i] * 10 + buff;
+      buff = 0;
+    }
+  }
+  set_scale(value, ++scale);
+}
+
+void div_by_ten(s21_decimal *value) {
+  int buff[3];
+  int scale = get_scale(*value);
+  for (int i = 2; i >= 0; i--) {
+    buff[i] = value->bits[i] % 10;
+    value->bits[i] /= 10;
+  }
+  for (int i = 2; i >= 1; i--) {
+    value->bits[i - 1] += buff[i] * pow(10, 8);
+  }
+  set_scale(value, ++scale);
+}
+
+void alignment(s21_decimal *value1, s21_decimal *value2) {
+  int mant_size1 = get_scale(*value1), mant_size2 = get_scale(*value2);
+  if (mant_size1 == mant_size2) return;
+  if (mant_size2 < mant_size1) {
+    alignment(value2, value1);
+    return;
+  }
+  while (mant_size1 < mant_size2) {
+    if ((*value1).bits[2] > 0xFFFFFFF / 10) break;
+    mul_by_ten(value1);
+    mant_size1++;
+  }
+
+  set_scale(value1, mant_size1);
+  while (mant_size1 < mant_size2) {
+    if ((*value2).bits[0] % 10 != 0) break;
+    div_by_ten(value2);
+    mant_size2--;
+  }
+
+  set_scale(value2, mant_size2);
+  while (mant_size1 < mant_size2) {
+    div_by_ten(value2);
+    mant_size2--;
+  }
+  set_scale(value2, mant_size2);
+}
+
+///
+///
+///
+///
 
 int s21_negate(s21_decimal value, s21_decimal *result) {
   enum { minus_bit = 0x80000000 };
@@ -105,12 +189,13 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
 // }
 
 bool set_scale(s21_decimal *value, uint8_t scale) {
-  if (scale > 28) return 1;
-  s21_decimal decimal_scale = (s21_decimal){{scale, 0, 0, 0}};
+  if (scale > 28) return 0;
+  bool sign = get_sign(*value);
 
-  for (int bit_i = scale_start; bit_i <= scale_end; ++bit_i)
-    set_bit(value, bit_i, get_bit(decimal_scale, bit_i % scale_start));
-  return 0;
+  value->bits[3] = (0b11111111 << 16) & scale << 16;
+  set_sign(value, sign);
+
+  return 1;
 }
 
 uint8_t get_scale(s21_decimal value) {
