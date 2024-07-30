9b5c30b9c238194c342659141a1194367067c9a8
jonnabin <jonnabin@student.21-school.ru>
2024-07-31 03:06:15 +0700

    fix err_code for calculation

--- decimal/s21_decimal.c
+++ decimal/s21_decimal.c
@@ -107,8 +107,12 @@ static bool add_word(uint32_t *result, uint32_t value_1, uint32_t value_2,
 }
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
+  *result = (s21_decimal){{0, 0, 0, 0}};
+  s21_decimal value1_orig = value_1;
+  s21_decimal value2_orig = value_2;
   const bool sign_1_orig = get_sign(value_1);
   const bool sign_2_orig = get_sign(value_2);
+
   /* чтобы узнать знак результата необходимо сравнить биты не учитывая знак */
   set_sign(&value_1, plus);
   set_sign(&value_2, plus);
@@ -116,8 +120,6 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
       (sign_1_orig == minus && sign_2_orig == minus) ||
       (sign_1_orig == minus && s21_is_greater(value_1, value_2)) ||
       (sign_2_orig == minus && s21_is_less(value_1, value_2));
-  if (result_sign) compliment2(*result, result);
-  set_sign(result, result_sign);
 
   // при необходимости переводим в дополнительный код.
   if (sign_1_orig == minus) {
@@ -139,12 +141,24 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
                       value_2.bits[2], transfer);
 
   uint8_t err_code = 0;
-  if (transfer == true &&
-      sign_1_orig == sign_2_orig) { /* проверка переполнения s21_decimal */
-    if (result_sign == plus) err_code = 1;
-    if (result_sign == minus) err_code = 2;
+  if (result_sign == minus) {
+    compliment2(*result, result);
   }
 
+  if (sign_1_orig == sign_2_orig) { /* проверка переполнения s21_decimal */
+    set_sign(&value1_orig, plus);
+    set_sign(&value2_orig, plus);
+    // если результат суммы положительных/отрицательных меньше/больше, значит
+    // ошибка переполнения.
+    if (s21_is_greater(value1_orig, *result) ||
+        s21_is_greater(value2_orig, *result)) {
+      err_code = 1;
+      if (result_sign) {
+        err_code = 2;
+      }
+    }
+  }
+  set_sign(result, result_sign);
   return err_code;
 }
 
@@ -157,9 +171,28 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   return err_code;
 }
 
+static void swap(s21_decimal *value_1, s21_decimal *value_2) {
+  s21_decimal buf = *value_1;
+  *value_1 = *value_2;
+  *value_2 = buf;
+}
+
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   const bool sign_1_orig = get_sign(value_1);
   const bool sign_2_orig = get_sign(value_2);
+
+  const bool result_sign = (sign_1_orig == minus || sign_2_orig == minus) &&
+                           !(sign_1_orig == minus && sign_2_orig == minus);
+  {
+    // оптимизация умножения :)
+    set_sign(&value_1, plus);
+    set_sign(&value_2, plus);
+    if (s21_is_greater(value_2, value_1)) {
+      swap(&value_1, &value_2);
+    }
+    set_sign(&value_1, sign_1_orig);
+    set_sign(&value_2, sign_2_orig);
+  }
 
   if (sign_1_orig == minus) set_sign(&value_1, plus);
   if (sign_2_orig == minus) set_sign(&value_2, plus);
@@ -170,10 +203,13 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   while (value_2.bits[0] | value_2.bits[1] | value_2.bits[2]) {
     (void)s21_sub(value_2, (s21_decimal){{1, 0, 0, 0}}, &value_2);
     err_code = s21_add(value_1, *result, result);
+    if (err_code) {
+      err_code = 1;
+      if (result_sign == minus) err_code = 2;
+      return err_code;
+    }
   }
 
-  const bool result_sign = (sign_1_orig == minus || sign_2_orig == minus) &&
-                           !(sign_1_orig == minus && sign_2_orig == minus);
   set_sign(result, result_sign);
 
   // TODO: ошибка
