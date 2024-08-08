2b53cc624bdab2d377015b6d030c211849e5b125
jonnabin <jonnabin@student.21-school.ru>
2024-08-09 02:16:30 +0700

    qq

--- big_decimal/Barithmetic.c
+++ big_decimal/Barithmetic.c
@@ -19,7 +19,7 @@ void Bcompliment2(big_decimal value, big_decimal *result) {
 bool Bdigits_div10(big_decimal *value) {
   // const uint8_t scale = Bget_scale(*value);
   const uint8_t err_code =
-      Bs21_div(*value, (big_decimal){{10, 0, 0, 0, 0, 0}}, value);
+      Bs21_div(*value, (big_decimal){{10, 0, 0, 0, 0, 0}}, value, div);
   // Bset_scale(value, scale - 1);
   return err_code;
 }
@@ -45,6 +45,8 @@ int Bincrement(big_decimal value, big_decimal *result) {
 }
 
 int Bdigits_add(big_decimal value_1, big_decimal value_2, big_decimal *result) {
+  const big_decimal value_1_orig = value_1;
+  const big_decimal value_2_orig = value_2;
   *result = (big_decimal){{0}};
   const bool sign_1_orig = Bget_sign(value_1);
   const bool sign_2_orig = Bget_sign(value_2);
@@ -73,15 +75,27 @@ int Bdigits_add(big_decimal value_1, big_decimal value_2, big_decimal *result) {
                          value_2.bits[i], transfer);
   }
 
-  uint8_t err_code = 0;
   if (result_sign == minus) {
     Bcompliment2(*result, result);
   }
+
+  uint8_t err_code = 0;
 
   if (result->bits[3] != 0) {
     err_code = 1;
     if (result_sign == minus) {
       err_code = 2;
+    }
+  }
+  if (sign_1_orig == sign_2_orig) { /* проверка переполнения s21_decimal */
+    // если результат суммы положительных/отрицательных меньше/больше, значит
+    // ошибка переполнения.
+    if (Bdigits_gt(value_1_orig, *result) ||
+        Bdigits_gt(value_2_orig, *result)) {
+      err_code = 1;
+      if (result_sign) {
+        err_code = 2;
+      }
     }
   }
   Bset_result_sign(result, result_sign);
@@ -99,6 +113,7 @@ int Bdigits_sub(big_decimal value_1, big_decimal value_2, big_decimal *result) {
 }
 
 int Bs21_mul(big_decimal value_1, big_decimal value_2, big_decimal *result) {
+  *result = (big_decimal){{0}};
   const bool sign_1_orig = Bget_sign(value_1);
   const bool sign_2_orig = Bget_sign(value_2);
 
@@ -136,7 +151,8 @@ int Bs21_mul(big_decimal value_1, big_decimal value_2, big_decimal *result) {
 }
 
 // https://en.wikipedia.org/wiki/Division_algorithm#Integer_division_(unsigned)_with_remainder
-int Bs21_div(big_decimal value_1, big_decimal value_2, big_decimal *result) {
+int Bs21_div(big_decimal value_1, big_decimal value_2, big_decimal *result,
+             uint8_t mode) {
   if (Bdigits_eq(value_2, (big_decimal){{0}})) /* если делим на 0 */
     return 3;
 
@@ -149,7 +165,7 @@ int Bs21_div(big_decimal value_1, big_decimal value_2, big_decimal *result) {
   Bset_sign(&value_2, plus);
 
   big_decimal Q = (big_decimal){{0}};  // частное quotient
-  big_decimal R = (big_decimal){{0}};  // остаток remainder
+  big_decimal R = (big_decimal){{0}};  // остаток reside
   while (1) {
     for (u_int8_t i = 0; i < 6; ++i) { /* сбрасывает значение Q */
       Q.bits[i] = 0;
@@ -164,8 +180,14 @@ int Bs21_div(big_decimal value_1, big_decimal value_2, big_decimal *result) {
         Bset_bit(&Q, i, 1);
       }
     }
-
+    if (mode == reside) {
+      *result = R;
+      return 0;
+    }
     *result = Q;
+    if (mode == whole) {
+      return 0;
+    }
 
     // домножаем на 10 и снова делим, пока остаток не станет 0.
     if (Bdigits_ne(R, (big_decimal){{0}})) {
@@ -189,46 +211,12 @@ int Bs21_div(big_decimal value_1, big_decimal value_2, big_decimal *result) {
   return err_code;
 }
 
-int Bs21_div2(big_decimal value_1, big_decimal value_2, big_decimal *result) {
-  if (Bdigits_eq(value_2, (big_decimal){{0}})) /* если делим на 0 */
-    return 2;
-
-  const bool sign_1 = Bget_sign(value_1);
-  const bool sign_2 = Bget_sign(value_2);
-  const bool result_sign = (sign_1 == minus || sign_2 == minus) &&
-                           !(sign_1 == minus && sign_2 == minus);
-  Bset_sign(&value_1, plus);
-  Bset_sign(&value_2, plus);
-
-  big_decimal Q = (big_decimal){{0}};  // частное quotient
-  big_decimal R = (big_decimal){{0}};  // остаток remainder
-
-  for (u_int8_t i = 0; i < 6; ++i) { /* сбрасывает значение Q */
-    Q.bits[i] = 0;
-  }
-  R = (big_decimal){{0}};
-
-  for (int i = BDIGITS_BIT_COUNT - 1; i >= 0; i--) {
-    Bleft_shift(&R);
-    Bset_bit(&R, 0, Bget_bit(value_1, i));
-    if (Bdigits_ge(R, value_2)) {
-      (void)Bdigits_sub(R, value_2, &R);
-      Bset_bit(&Q, i, 1);
-    }
-  }
-
-  *result = Q;
-  Bset_result_sign(result, result_sign);
-
-  return 0;
-}
-
 int Bmod(big_decimal value_1, big_decimal value_2) {
   Bset_sign(&value_1, plus);
   Bset_sign(&value_2, plus);
 
   big_decimal Q = (big_decimal){{0}};  // частное quotient
-  big_decimal R = (big_decimal){{0}};  // остаток remainder
+  big_decimal R = (big_decimal){{0}};  // остаток reside
 
   for (u_int8_t i = 0; i < 6; ++i) { /* сбрасывает значение Q */
     Q.bits[i] = 0;
--- decimal/arithmetic.c
+++ decimal/arithmetic.c
@@ -2,43 +2,8 @@
 
 void magnitude_div10(s21_decimal *value) {
   big_decimal big = (big_decimal){{0}};
-  Bs21_div(big, (big_decimal){{10}}, &big);
+  Bs21_div(big, (big_decimal){{10}}, &big, div);
   big_to_decimal(big, value);
-}
-
-int whole_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-  big_decimal big_1 = (big_decimal){{0}};
-  big_decimal big_2 = (big_decimal){{0}};
-  big_decimal big_result = (big_decimal){{0}};
-  uint8_t err_code = 0;
-  *result = (s21_decimal){{0}};
-
-  if (!is_correct_scale(value_1) && !is_correct_scale(value_2)) {
-    err_code = 4;
-  }
-
-  if (err_code == 0) {
-    decimal_to_big(value_1, &big_1);
-    decimal_to_big(value_2, &big_2);
-    const uint8_t scale_1 = Bget_scale(big_1);
-    const uint8_t scale_2 = Bget_scale(big_2);
-
-    uint8_t err_code = Bs21_div(big_1, big_2, &big_result);
-    if (err_code) {
-      return err_code;
-    }
-    int8_t scale_result = scale_1 - scale_2 + Bget_scale(big_result);
-
-    while (scale_result < 0) {
-      Bdigits_mul10(&big_result);
-      ++scale_result;
-    }
-    Bset_scale(&big_result, scale_result);
-    Bcircumcision(&big_result);
-    err_code = big_to_decimal(big_result, result);
-  }
-
-  return err_code;
 }
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
@@ -48,9 +13,11 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   uint8_t err_code = 0;
   *result = (s21_decimal){{0}};
 
-  if ((!is_correct_scale(value_1) && !is_correct_scale(value_2))) {
-    err_code = 4;
+  err_code = check_scale(value_1);
+  if (err_code == 0) {
+    err_code = check_scale(value_2);
   }
+
   if (err_code == 0) {
     decimal_to_big(value_1, &big_1);
     decimal_to_big(value_2, &big_2);
@@ -80,8 +47,9 @@ int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   *result = (s21_decimal){{0}};
   uint8_t err_code = 0;
 
-  if (!is_correct_scale(value_1) && !is_correct_scale(value_2)) {
-    err_code = 4;
+  err_code = check_scale(value_1);
+  if (err_code == 0) {
+    err_code = check_scale(value_2);
   }
 
   if (err_code == 0) {
@@ -112,8 +80,9 @@ int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   uint8_t err_code = 0;
   *result = (s21_decimal){{0}};
 
-  if (!is_correct_scale(value_1) && !is_correct_scale(value_2)) {
-    err_code = 4;
+  err_code = check_scale(value_1);
+  if (err_code == 0) {
+    err_code = check_scale(value_2);
   }
 
   if (err_code == 0) {
@@ -122,7 +91,7 @@ int s21_div(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
     const u_int8_t scale_1 = Bget_scale(big_1);
     const u_int8_t scale_2 = Bget_scale(big_2);
 
-    uint8_t err_code = Bs21_div(big_1, big_2, &big_result);
+    uint8_t err_code = Bs21_div(big_1, big_2, &big_result, div);
     if (err_code) {
       return err_code;
     }
