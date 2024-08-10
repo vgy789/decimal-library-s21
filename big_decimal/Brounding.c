3ab1ca445a93b4a38f3ad2ac66b918f72158e165
jonnabin <jonnabin@student.21-school.ru>
2024-08-10 17:54:41 +0700

    fix merge moraleas: floor func

--- big_decimal/Brounding.c
+++ big_decimal/Brounding.c
@@ -18,6 +18,7 @@ int Bs21_round(big_decimal value, big_decimal *result) {
   Bset_scale(&value, count - 1);
   big_decimal last_digit = {{0}};
   Bs21_truncate(value, &last_digit);
+
   if (Bmod(last_digit, (big_decimal){{10}}) >= 5) {
     if (Bget_sign(temp) == plus) {
       Bdigits_add(temp, (big_decimal){{1}}, result);
@@ -30,16 +31,18 @@ int Bs21_round(big_decimal value, big_decimal *result) {
   return 0;
 }
 
-int s21_floor(s21_decimal value, s21_decimal *result) {
-uint8_t err_code = 0;
-  if (!is_correct_scale(value)) {
-    err_code = 4;
+int Bs21_floor(big_decimal value, big_decimal *result) {
+  int count = Bget_scale(value);
+  big_decimal temp = {{0}};
+  Bs21_truncate(value, &temp);
+  Bset_scale(&value, count - 1);
+  big_decimal last_digit = {{0}};
+  Bs21_truncate(value, &last_digit);
+
+  if (Bget_sign(temp) == minus) {
+    Bdigits_sub(temp, (big_decimal){{1}}, result);
+  } else {
+    *result = temp;
   }
-  if (err_code == 0) {
-    big_decimal big = (big_decimal){{0}};
-    decimal_to_big(value, &big);
-    Bs21_floor(big, &big);
-    err_code = big_to_decimal(big, result);
-  }
-  return err_code;
+  return 0;
 }
--- decimal/rounding.c
+++ decimal/rounding.c
@@ -24,17 +24,13 @@ int s21_round(s21_decimal value, s21_decimal *result) {
   return err_code;
 }
 
-int Bs21_floor(big_decimal value, big_decimal *result) {
-  int count = Bget_scale(value);
-  big_decimal temp = {{0}};
-  Bs21_truncate(value, &temp);
-  Bset_scale(&value, count - 1);
-  big_decimal last_digit = {{0}};
-  Bs21_truncate(value, &last_digit);
-  if (Bget_sign(temp) == minus) {
-      Bdigits_sub(temp, (big_decimal){{1}}, result);
-  } else {
-  *result = temp;
+int s21_floor(s21_decimal value, s21_decimal *result) {
+  uint8_t err_code = check_scale(value);
+  if (err_code == 0) {
+    big_decimal big = (big_decimal){{0}};
+    decimal_to_big(value, &big);
+    Bs21_floor(big, &big);
+    err_code = big_to_decimal(big, result);
   }
-  return 0;
+  return err_code;
 }
